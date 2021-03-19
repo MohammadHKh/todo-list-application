6d621b163debe37738272a5454042b19bfb587d6
Mohammad <mohammad.khezrian@gmail.com>
2021-03-19 18:10:24 +0100

    Use Validation static methods in addTask, editTask, markAsDone, and updateTask methods in ToDoList class.

--- src/main/java/model/ToDoList.java
+++ src/main/java/model/ToDoList.java
@@ -95,35 +95,33 @@ public class ToDoList extends UserCommand {
 
         System.out.println("Enter The Task Title:");
         taskTilte = input.nextLine();
-        if (taskTilte.isBlank() || taskTilte.isEmpty()) {
-            System.out.println("Error: Task Title Can Not Be Null");
-            return;
-        }
 
         System.out.println("Enter The Task Due Date In The Format YYYY-MM-DD:");
         String taskDueDate = input.nextLine();
-        try {
-            dueDate = new SimpleDateFormat("yyyy-mm-dd").parse(taskDueDate);
-        } catch (ParseException e) {
-            System.out.println("Error: Please Enter Date in Correct Format: YYYY-MM-DD");
-            return;
-        }
 
         System.out.println("Enter The Task Status In The Correct Format: DONE or InProgress (Case Insensitive) :");
         String taskStatus = input.nextLine();
-        if (!taskStatus.equalsIgnoreCase("DONE") && !taskStatus.equalsIgnoreCase("InProgress")) {
-            System.out.println("Error: Please Enter The Task Status In The Correct Format (Case Insensitive): DONE or InProgress :");
-            return;
-        }
 
         System.out.println("Enter Project Title:");
         taskProjectTitle = input.nextLine();
-        if (taskProjectTitle.isBlank() || taskDueDate.isEmpty()) {
-            System.out.println("Error: Project Title Can Not Be Null");
+
+        try{
+            Validation.validateStringField(taskTilte);
+            Validation.validateStringField(taskProjectTitle);
+            Validation.validateTaskStatus(taskStatus);
+            Validation.validateStringField(taskDueDate);
+            dueDate = new SimpleDateFormat("yyyy-mm-dd").parse(taskDueDate);
+            tasklist.add(new Task(taskTilte, taskProjectTitle, taskStatus, dueDate));
+        }
+        catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
             return;
         }
-
-        tasklist.add(new Task(taskTilte, taskProjectTitle, taskStatus, dueDate));
+        catch (ParseException e){
+            System.out.println("Error: Please Enter Date in Correct Format: YYYY-MM-DD ");
+            return;
+        }
+        System.out.println("Task added Successfully");
     }
 
     public void editTask() {
@@ -131,24 +129,25 @@ public class ToDoList extends UserCommand {
         System.out.println("Please Enter Task Number:");
         Scanner input = new Scanner(System.in);
         int taskNumber = input.nextInt();
-        if (taskNumber < 0 || taskNumber > tasklist.size() - 1) {
-            System.out.println("Error, Please Enter Valid Task Number, There is no task-" + taskNumber);
-            return;
+        try{
+            Validation.validateTaskListIndex(tasklist,taskNumber);
+            System.out.println("Please Enter 1- Update  2-Mark As Down  3-Remove");
+            Scanner userInput = new Scanner(System.in);
+            int updateType = userInput.nextInt();
+            switch (updateType) {
+                case 1:
+                    updateTask(taskNumber);
+                    break;
+                case 2:
+                    markAsDone(taskNumber);
+                    break;
+                case 3:
+                    removeTask(taskNumber);
+                    break;
+            }
         }
-
-        System.out.println("Please Enter 1- Update  2-Mark As Down  3-Remove");
-        Scanner userInput = new Scanner(System.in);
-        int updateType = userInput.nextInt();
-        switch (updateType) {
-            case 1:
-                updateTask(taskNumber);
-                break;
-            case 2:
-                markAsDown(taskNumber);
-                break;
-            case 3:
-                removeTask(taskNumber);
-                break;
+        catch(Exception e){
+            System.out.println(e.getMessage());
         }
     }
 
@@ -157,18 +156,19 @@ public class ToDoList extends UserCommand {
         System.out.println(("Task-" + taskNumber + "Successfully Removed"));
     }
 
-    private void markAsDown(int taskNumber) {
+    private void markAsDone(int taskNumber) {
         String taskStatus = null;
         Task task = tasklist.get(taskNumber);
         System.out.println("Enter Task Status In The Format DONE or InProgress (Case Insensitive) :");
         Scanner input = new Scanner(System.in);
         taskStatus = input.nextLine();
-
-
-        if (taskStatus.equalsIgnoreCase("DONE") || taskStatus.equalsIgnoreCase("InProgress")) {
+        try{
+            Validation.validateTaskStatus(taskStatus);
             task.setStatus(taskStatus);
             System.out.println(("Task Updated successfully"));
-            return;
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
         }
         System.out.println("Task Is Not Modified");
     }
@@ -176,38 +176,37 @@ public class ToDoList extends UserCommand {
     private void updateTask(int taskNumber) {
         Date dueDate = null;
         String stringDate = null;
+        boolean isUpdated = false;
+
         Task task = tasklist.get(taskNumber);
         System.out.println("Enter the details");
-
         System.out.println("Enter Task Title:");
         Scanner input = new Scanner(System.in);
         String title = input.nextLine();
-        boolean isUpdated = false;
-
-        if (!title.isBlank() && !title.isEmpty()) {
-            task.setTitle(title);
-            isUpdated = true;
-        }
-
         System.out.println("Enter Task Project Title:");
         String projectTitle = input.nextLine();
-        if (!projectTitle.isBlank() && !projectTitle.isEmpty()) {
-            task.setProjectName(projectTitle);
-            isUpdated = true;
-        }
 
         System.out.println("Enter The Task Due Date In The Format YYYY-MM-DD:");
         stringDate = input.nextLine();
-        if (!stringDate.isEmpty() && !stringDate.isBlank())
-            try {
-                dueDate = new SimpleDateFormat("yyyy-mm-dd").parse(stringDate);
-                task.setDueDate(dueDate);
-                isUpdated = true;
-            } catch (ParseException e) {
-                System.out.println("Error: Please Enter Date in Correct Format: YYYY-MM-DD ");
-                return;
-            }
 
+        try{
+            Validation.validateStringField(title);
+            Validation.validateStringField(projectTitle);
+            Validation.validateStringField(stringDate);
+            dueDate = new SimpleDateFormat("yyyy-mm-dd").parse(stringDate);
+            task.setTitle(title);
+            task.setProjectName(projectTitle);
+            task.setDueDate(dueDate);
+            isUpdated = true;
+        }
+        catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return;
+        }
+        catch (ParseException e){
+            System.out.println("Error: Please Enter Date in Correct Format: YYYY-MM-DD ");
+            return;
+        }
         System.out.println(("Task" + (isUpdated ? "Updated Successfully" : "Is Not Modified")));
     }
 
